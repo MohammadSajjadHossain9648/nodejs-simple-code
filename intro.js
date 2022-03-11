a04a7c9ac8e9e506f538d9b4fde8107b991dc272
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2022-03-11 23:57:23 +0600

    introduction code

---
+++ intro.js
@@ -0,0 +1,10 @@
+console.log(`hey it's my first node app!!!`);
+
+const value = 90;
+
+if (value < 50) {
+    console.log(value + ' is a small number');
+} else {
+    console.log(value + ' is a large number');
+}
+
